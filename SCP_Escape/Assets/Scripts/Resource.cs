63691d60533f07a39b9e7928ddde49af472d0867
Amazeing Games <AmazeingGameDesign@gmail.com>
2023-02-09 14:00:03 -0800

    Created Card Prefab + Scriptable Objects
    Created the card prefab and the scriptable objects for each individual resource card that contains the data for things like art, type, color, initial, and more to be added.

--- SCP_Escape/Assets/Scripts/Resource.cs
+++ SCP_Escape/Assets/Scripts/Resource.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+[CreateAssetMenu(menuName = "Cards/Resources")]
 
 public class Resource : ScriptableObject
 {
     public enum CardType { Anomaly, Escapee, Prisoner, Food, Key, Misfortune }
 
     [SerializeField] CardType ECardType;
-    [SerializeField] Sprite sprite;
+    [SerializeField] Sprite symbol;
+    [SerializeField] Color cardColor;
+    [SerializeField] char initial;
 
     public bool HasBeenPlayed { get; protected set; }
     public bool HasBeenConsumed { get; protected set; }
